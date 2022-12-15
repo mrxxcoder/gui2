6c265b0603acfc340fcd1f5b1cb621dd5ff7c5ab
Ahmed Samy <52106004+mrxxcoder@users.noreply.github.com>
2022-12-15 18:59:33 +0200

    Add files via upload

---
+++ proj/src/main/java/com/mycompany/proj/Proj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proj;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class Proj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
